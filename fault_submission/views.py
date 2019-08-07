71b175e79fa40c0bb3b365bfef9de677bcc4b723
johnbenedictyan <johnbenedictyan@gmail.com>
2019-08-07 14:08:38 +0000

    Added in a custom form error handler to handle the expired csrf token.

--- fault_submission/views.py
+++ fault_submission/views.py
@@ -1,10 +1,17 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render,redirect,render_to_response
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from .models import fault
 from .forms import fault_form
 from condo_fault_project import settings
 
+def csrf_failure(request, reason=""):
+    messages.error(
+                request,
+                "Login form has expired. Please try again."
+            )
+    return redirect("/")
+    
 def fault_submission(request):
     if request.method=="GET":
         new_fault_form = fault_form()
@@ -36,3 +43,4 @@ def fault_submission(request):
                     "fault_form":dirty_fault_form
                 }
             )
+            
